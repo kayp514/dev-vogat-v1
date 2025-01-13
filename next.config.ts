22f6c9b02b622e378a02629acf0e29c2c5050ac6
kayp514 <pa.ka12@outlook.com>
2025-01-13 14:57:39 -0500

    Refactor Next.js configuration: remove old config file and add new TypeScript config with rewrites. The new configuration includes a console removal setting for production and a rewrite rule for authentication paths.

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    compiler: {
+        removeConsole: process.env.NODE_ENV === 'production',
+      },
+      
+      async rewrites() {
+        return [
+          {
+            source: '/__/auth/:path*',
+            destination: 'https://dev-coffeeconnect-v1.firebaseapp.com/__/auth/:path*',
+          },
+        ]
+      },
+    };
+
+export default nextConfig;
