c7ace5172c22eda7d769f180784613e6952c1830
kayp514 <pa.ka12@outlook.com>
2025-12-03 20:20:24 -0500

    fix: update ternUIUrl to latest version and clean up import syntax in VzeroPage component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
             isTokenAutoRefreshEnabled: true,
           }}
           //apiUrl="ternsecure-auth-admin.vercel.app"
-          ternUIUrl="https://cdn.jsdelivr.net/npm/@tern-secure/auth@1.1.0-canary.v20251203153858/dist/ternsecure.browser.js"
+          ternUIUrl="https://cdn.jsdelivr.net/npm/@tern-secure/auth@1.1.0-canary.v20251204011814/dist/ternsecure.browser.js"
           persistence="browserCookie"
           requiresVerification={false}
         >
